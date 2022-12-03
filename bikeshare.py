22c45a7f5eff6edb362e1c6f76043f9c6fb49be4
Jowharah <jowharah.m@gmail.com>
2022-12-03 12:27:27 +0300

    edit the bikeshare file

--- bikeshare.py
+++ bikeshare.py
@@ -5,7 +5,7 @@ import time
 import pandas as pd
 import numpy as np
 
-#Global values 
+#Global values to be used in all funtions 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
